a5616de2303f8c8d05c6c8f97c3d64b61079fb9f
Christian Kuechler <ckuechler@gmail.com>
2024-04-29 15:01:31 +0200

    Lab done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => (a - b));
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(typeof this.items[pos] === 'undefined') { 
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("Empty SortedList");
+    } else {
+      return Math.max( ...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("Empty SortedList");
+    } else {
+      return Math.min( ...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((acc, val) => {
+      return acc + val;
+    },0);
 
-  avg() {}
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("Empty SortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
